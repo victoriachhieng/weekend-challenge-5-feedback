7255ccff788afbd2811999b9e2a3b571af36b458
Victoria Chhieng <victoria_chhieng@hotmail.com>
2018-12-08 13:34:41 -0600

    add css to Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
+import './Navbar.css';
 
 class Navbar extends Component {
 
     render(){
         return(
             <div>
-            <h1>Feedback!</h1>
+            <h1 className="navbar-title">Feedback!</h1>
             </div>
         )
     }
