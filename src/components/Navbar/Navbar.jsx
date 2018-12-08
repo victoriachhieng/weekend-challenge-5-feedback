77f7598e00b5c22625f9feb59a74a2840ac2794f
Victoria Chhieng <victoria_chhieng@hotmail.com>
2018-12-08 13:22:04 -0600

    Navbar component created and imported in App.js

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Navbar extends Component {
+
+    render(){
+        return(
+            <div>
+            <h1>Feedback!</h1>
+            </div>
+        )
+    }
+}
+
+export default Navbar;
