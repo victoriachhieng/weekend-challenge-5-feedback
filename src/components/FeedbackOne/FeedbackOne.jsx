f7f5b7cd703ba579eb545a4b4ecd1d5f6894a19c
Victoria Chhieng <victoria_chhieng@hotmail.com>
2018-12-09 16:39:23 -0600

    add reducer andcomponent FeedbackOne

--- src/components/FeedbackOne/FeedbackOne.jsx
+++ src/components/FeedbackOne/FeedbackOne.jsx
@@ -1,18 +1,36 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
 
 class FeedbackOne extends Component {
-    render(){
-        return(
-            <div>
-            <h5>1 of 4 pages</h5>
-                <br/>
-            <h4>How are you feeling today?</h4>
-            <input type="text" placeholder="response"/>
-            <br/>
-            <button>Next</button>
-            </div>
-        )
+
+  state = {
+    feeling: 0
+  }
+
+  handleChangeFeelings = event => {
+    console.log("in handleChange");
+    this.setState({
+      feeling: event.target.value
+    })
+  }
+
+  handleClick = () => {
+      this.props.dispatch({ type: "ADD_FEELING", payload: this.state })
     }
+
+  render() {
+    return (
+      <div>
+        <h5>1 of 4 pages</h5>
+        <br />
+        <h4>How are you feeling today?</h4>
+        <input onChange={this.handleChangeFeelings} type="number" placeholder="response"
+        />
+        <br />
+        <button onClick={this.handleClick}>Next</button>
+      </div>
+    )
+  }
 }
 
-export default FeedbackOne;
+export default connect()(FeedbackOne);
